56749980d185b0584495ec758100667c2a9367e8
thanlim <117689952+thanlim@users.noreply.github.com>
2023-04-02 12:37:17 -0400

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,7 @@ int main()
 {
   cout << "Hello, my name is Than Lim" <<endl;
   cout << "Hello from the otherside of heaven"<<endl;
+  cout << "My friends and I are trying to create snake game"<<endl;
   return 0;
   
 }
